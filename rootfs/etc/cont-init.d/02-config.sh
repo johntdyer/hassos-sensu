7758300e9f3bfa8cb9d11e0a7882a69a09e904ae
John Dyer <johntdyer@gmail.com>
2021-09-07 11:14:24 -0400

    Update test to s6

--- rootfs/etc/cont-init.d/02-config.sh
+++ rootfs/etc/cont-init.d/02-config.sh
@@ -25,6 +25,6 @@ CONFIG="/etc/sensu/sensu-agent.conf"
 bashio::log.info "Configuring Sensu..."
 tempio \
     -conf /data/options.json \
-    -template /usr/share/tempio/sensu.jinja2 \
-    -out "${CONFIG}"
+    -template usr/share/tempio/sensu.gtpl \
+    -out test.json
 
